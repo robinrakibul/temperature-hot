6e907ceb292b0fb1b852963f89e60a3887942e03
robinrakibul <robinrakibulhassan@gmail.com>
2022-03-01 23:15:17 +0600

    finished weather project

--- js/app.js
+++ js/app.js
@@ -1,13 +1,23 @@
 const API_KEY = `7ff73be04f31431b98c908175b332390`;
 const searchTemperature = () => {
     const city = document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
     fetch(url)
     .then(res => res.json())
     .then(data => displayTemperature(data))
 }
 
+const setInnerText = (id,text) =>{
+    document.getElementById(id).innerText = text;
+}
 const displayTemperature = temperature =>{
-    console.log(temperature);
+    // console.log(temperature);
+    setInnerText('city',temperature.name);
+    setInnerText('temperature',temperature.main.temp);
+    setInnerText('weather-condition',temperature.weather[0].main);
+    // set weather icon
+    const url =`http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src',url);
 }
